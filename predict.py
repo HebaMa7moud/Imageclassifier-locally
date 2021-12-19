b41a81d77dd0d1e382f3b6fc7128931123138cd7
HebaMa7moud <81697137+HebaMa7moud@users.noreply.github.com>
2021-12-20 00:39:39 +0200

    Update predict.py

--- predict.py
+++ predict.py
@@ -10,21 +10,9 @@ import argparse
 import random
 import predict_func
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--path_dir',  action='store', default='checkpoint.pth')
-parser.add_argument('--gpu', action='store', default='gpu')
-parser.add_argument('--cat_nam', action ='store', default = 'cat_to_name.json')
-parser.add_argument('--img_dir', action='store', default='flowers/test/1/image_06743.jpg')
-parser.add_argument('--top_k', action='store', type= int, default= 5)
 
-args = parser.parse_args()
-checkpoint_path = args.path_dir
-device= args.gpu
-categ_nam = args.cat_nam
-directory = args.img_dir
-top_k = args.top_k
-
-model = predict_func.load_checkpoint(checkpoint_path)
+checkpoint_path, device, categ_nam, directory, top_k = predict_func.parse_arguments()
+model = predict_func.load_checkpoint(checkpoint_path,device)
 processed_image = predict_func.process_image(directory)
 top_probs, top_class, flowers_name, cat_to_name = predict_func.predict(device, categ_nam, directory, model, top_k)
 predict_func.print_flow_prob(directory, cat_to_name, top_probs, top_class, flowers_name)
